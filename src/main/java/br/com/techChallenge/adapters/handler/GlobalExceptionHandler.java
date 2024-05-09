f6aa232d1cfa963e5749c29e4d31463591e204c1
Guilherme Costa Sales <guilhermecosales@gmail.com>
2024-05-09 18:44:41 -0300

    feat: handle with method argument not valid exception

--- src/main/java/br/com/techChallenge/adapters/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/techChallenge/adapters/handler/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ import br.com.techChallenge.core.exceptions.category.ExistProductInCategory;
 import br.com.techChallenge.core.exceptions.order.PersonInOrderNotFound;
 import br.com.techChallenge.core.exceptions.order.ProductInOrderNotFound;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.LinkedHashMap;
@@ -52,6 +56,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         List<String> errors = ex.getConstraintViolations().stream()
                 .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .toList();
+
+        body.put(ERRORS, errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(MESSAGE, "Invalid request content.");
+
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
                 .toList();
 
         body.put(ERRORS, errors);
